143d56d43a7b474b0e93f3e4b3eb09ba93a6b987
cmunhozprog <cmunhozprog03@hotmail.com>
2021-01-30 09:13:40 -0300

    Create Project

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('content')
+    <div class="container">
+      <div class="row">
+
+      </div>
+      <h1 class="display-4">Olá livewire!</h1>
+    </div>
+@endsection
